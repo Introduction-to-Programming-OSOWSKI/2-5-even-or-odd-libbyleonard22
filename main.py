6b352fd764b8714980254205439762531f1c7ccf
libbyleonard22 <815989@mystma.org>
2021-10-01 14:36:32 -0500

    1

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 #WRITE YOUR CODE IN THIS FILE
+def isEven(n):
+    n = 1
+    
+    if x % 2 == 0:
+        print (n)
+        print ("even")
+    else:
+        print (n)
+        print ("odd")
+    x = x + 1
+isEven(n)
